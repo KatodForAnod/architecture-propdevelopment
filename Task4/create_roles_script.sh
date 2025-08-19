a29868619202c4d404c180b48b8a97880e773a25
Nikita Makarchuk <nikita.makarchuk@onlinesup.com>
2025-08-19 22:59:40 +0300

    task5

--- Task4/create_roles_script.sh
+++ Task4/create_roles_script.sh
@@ -6,7 +6,3 @@ kubectl create namespace home-guard-development
 kubectl apply -f clusterrolereader.yaml
 kubectl apply -f clusterrolewriter.yaml
 kubectl apply -f role_developer_houses_namespace.yaml
-
-kubectl apply -f clusterrolereaderbinding.yaml
-kubectl apply -f clusterrolewriterbinding.yaml
-kubectl apply -f role_developer_houses_namespace.yaml
